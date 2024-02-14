c319388eaf6b4a018b5b32975cb1428dca948802
Maximilien Koch Le Breton <themaxklb@gmail.com>
2024-02-14 18:29:17 +0100

    .h

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 18:26:19 by makoch-l          #+#    #+#             */
+/*   Updated: 2024/02/14 18:29:04 by makoch-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINT_H
+# define FT_PRINTF_H
+
+# include "libft/libft.h"
+
+int	ft_printf(const char *format, ...);
+int	print_unsigned(unsigned int nb);
+int	print_string(char *str);
+int	print_int(int n);
+int	print_char(char c);
