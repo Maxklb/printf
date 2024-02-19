c4a7c862b9af5e1da7a263ba0b6d8b62ed9c051e
Maximilien Koch Le Breton <themaxklb@gmail.com>
2024-02-19 17:53:36 +0100

    q

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 18:26:19 by makoch-l          #+#    #+#             */
-/*   Updated: 2024/02/19 17:48:51 by makoch-l         ###   ########.fr       */
+/*   Updated: 2024/02/19 17:53:28 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ int	print_string(char *str);
 int	print_int(int n);
 int	print_char(char c);
 int	print_hex(unsigned int value, int asc);
-int	print_pointer(unsigned long value, int asc)
+int	print_pointer(unsigned long value, int asc);
 
 #endif
